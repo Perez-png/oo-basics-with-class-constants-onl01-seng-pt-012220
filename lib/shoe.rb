eddd8499bdc3c30cb93b846e6cf4125857bc9042
Xavier Perez <xperez007@gmail.com>
2020-02-06 18:45:38 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,6 +2,27 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
   def initialize(brand)
     @brand = brand
   end
